b8195ffbdf2b5624d59ce0a83617452b375645de
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-03-14 15:19:47 -0500

    Coursework

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,14 +1,11 @@
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login.jsp")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/login.jsp").forward(request, response);
@@ -18,14 +15,17 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        HttpSession session = request.getSession();
+
         if(username.equals("username") && password.equals("password")){
+            session.setAttribute("user", true);
             Cookie loginCookie = new Cookie("user", username);
             //setting cookie to expire in 5 mins
             loginCookie.setMaxAge(5*60);
             response.addCookie(loginCookie);
-            response.sendRedirect("/profile.jsp");
+            response.sendRedirect("/profile");
         }else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login");
             PrintWriter out = response.getWriter();
             out.println("<font color=red>Either user name or password is wrong.</font>");
             try {
